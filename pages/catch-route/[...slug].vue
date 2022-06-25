b94b7add8a473bfc29b70328c5768825c3d3f372
Wahyu Febrianto <febriantowahyu63@gmail.com>
2022-06-25 22:42:35 +0700

    feat: creating page to catch all route on spesific path

---
+++ pages/catch-route/[...slug].vue
@@ -0,0 +1,5 @@
+<template>
+  <h1>Catch All Route page</h1>
+  <h2>The example of catch all route from the <i><u>catch-route</u></i> path</h2>
+  <p>The route params : {{ $route.params }}</p>
+</template>
---
+++ pages/catch-route/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Catch Route Page</h1>
+</template>
