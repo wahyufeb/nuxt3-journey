c187170dac84c0c2ff0e42234ca03ad6dd48a96f
Wahyu Febrianto <febriantowahyu63@gmail.com>
2022-06-30 21:38:54 +0700

    feat: add page metadata

---
+++ pages/page-metadata/index.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { title, description } from "@/constants/meta"
+definePageMeta({
+  title,
+  description
+})
+const route = useRoute()
+</script>
+
+<template>
+  <div>
+    <h1>Welcome to Page Metadata</h1>
+    <p>Meta title : {{route.meta.title}}</p>
+    <p>Meta description : {{route.meta.title}}</p>
+  </div>
+</template>
